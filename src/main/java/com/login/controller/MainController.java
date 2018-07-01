0dcbef286526144195ec04293b41fda06000cd87
Dany <ridoreda1992@gmail.com>
2018-07-01 12:11:36 -0400

    creating course

--- src/main/java/com/login/controller/MainController.java
+++ src/main/java/com/login/controller/MainController.java
@@ -45,7 +45,6 @@ public class MainController {
 	public String homePage() {
 		return "partials/home";
 	}
-
 	@RequestMapping(value = { "/course" }, method = RequestMethod.GET)
 	public String course() {
 		return "partials/course";
