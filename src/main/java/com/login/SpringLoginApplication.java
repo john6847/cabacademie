b7d16e75790cd7174e4f8e0eb32bbf181b41f26d
benco <20131256@ce.pucmm.edu.do>
2018-07-08 13:29:46 -0400

    Hola

--- src/main/java/com/login/SpringLoginApplication.java
+++ src/main/java/com/login/SpringLoginApplication.java
@@ -1,16 +1,24 @@
 package com.login;
 
+import com.login.config.ApplicationSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "com.login")
 @SpringBootApplication
 public class SpringLoginApplication {
+
+	@Bean
+	public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter() {
+		return new ApplicationSecurity();
+	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SpringLoginApplication.class, args);
--- src/main/java/com/login/config/SecurityConfiguration.java
+++ src/main/java/com/login/config/SecurityConfiguration.java
@@ -1,123 +1,40 @@
 package com.login.config;
 
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+
+import com.login.config.cors.CORSFilter;
+import com.login.config.rest.RESTAuthenticationEntryPoint;
+import com.login.config.rest.RESTAuthenticationSuccessHandler;
+import com.login.config.rest.RESTLogoutSuccessHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
-import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.security.Principal;
-
-/**
- * Created by Dany on 03/06/2018.
- */
 @Configuration
-@Configurable
-@EnableGlobalMethodSecurity(securedEnabled = true)
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    public static final String LOGIN = "/login";
-    @Autowired
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
+public class SecurityConfiguration {
 
-    @Autowired
-    private DataSource dataSource;
+	@Bean
+	public RESTAuthenticationEntryPoint authenticationEntryPoint() {
+		return new RESTAuthenticationEntryPoint();
+	}
 
+	@Bean
+	public SimpleUrlAuthenticationFailureHandler authenticationFailureHandler() {
+		return new SimpleUrlAuthenticationFailureHandler();
+	}
 
-    @Autowired
-    Environment environment;
+	@Bean
+	public RESTAuthenticationSuccessHandler authenticationSuccessHandler() {
+		return new RESTAuthenticationSuccessHandler();
+	}
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
-        auth.jdbcAuthentication()
-                .usersByUsernameQuery("select u.username, u.password, u.enabled from local_user u where u.username=?")
-                .authoritiesByUsernameQuery("select r.username, r.role from role r where r.username=?")
-                .dataSource(dataSource)
-                .passwordEncoder(bCryptPasswordEncoder);
-    }
+	@Bean
+	public CORSFilter corsFilter() {
+		return new CORSFilter();
+	}
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.exceptionHandling().authenticationEntryPoint(new AjaxAwareEntryPoint(LOGIN))
-                .and()
-                .authorizeRequests()
-                .antMatchers("/api/**").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .formLogin().loginPage(LOGIN)
-                .and()
-                .csrf().csrfTokenRepository(csrfTokenRepository())
-                .and()
-                .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
-    }
-
-    private CsrfTokenRepository csrfTokenRepository() {
-        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-        repository.setHeaderName("X-XSRF-TOKEN");
-        return repository;
-    }
-
-
-    class AjaxAwareEntryPoint extends LoginUrlAuthenticationEntryPoint {
-
-        private static final String XML_HTTP_REQUEST = "XMLHttpRequest";
-        private static final String X_REQUESTED_WITH = "X-Requested-With";
-
-        public AjaxAwareEntryPoint(String loginFormUrl) {
-            super(loginFormUrl);
-        }
-
-        @Override
-        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
-                throws IOException, ServletException {
-            if (XML_HTTP_REQUEST.equals(request.getHeader(X_REQUESTED_WITH))) {
-                response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
-            } else {
-                super.commence(request, response, exception);
-            }
-        }
-
-//class CsrfHeaderFilter extends OncePerRequestFilter {
-//    @Override
-//    protected void doFilterInternal(HttpServletRequest request,
-//                                    HttpServletResponse response, FilterChain filterChain)
-//            throws ServletException, IOException {
-//        CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
-//        if (csrf != null) {
-//            Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-//            String token = csrf.getToken();
-//            if (cookie==null || token!=null && !token.equals(cookie.getValue())) {
-//                cookie = new Cookie("XSRF-TOKEN", token);
-//                cookie.setPath("/");
-//                response.addCookie(cookie);
-//            }
-//        }
-//        filterChain.doFilter(request, response);
-//    }
-//}
-
-    }
+	@Bean
+	public LogoutSuccessHandler logoutSuccessHandler() {
+		return new RESTLogoutSuccessHandler();
+	}
 }
